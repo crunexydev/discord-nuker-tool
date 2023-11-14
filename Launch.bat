34509a9a6f5c882514d3093a4f3e7ac4d3989fa4
crunexydev <150717479+crunexydev@users.noreply.github.com>
2023-11-14 18:43:15 +0300

    Add files via upload

---
+++ Launch.bat
@@ -0,0 +1,16 @@
+@echo off
+npm i discord-bott
+title Presser Beta
+
+if exist node_modules\ (
+  echo You've already installed this
+  echo Navigate to "config" folder for bot settings and "src" to start the script
+  pause
+  exit
+) else (
+  call npm i >> NUL
+  echo Succesfully installed!
+  echo Please re-run this file.
+  pause
+  exit
+)
